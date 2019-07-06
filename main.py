892b4bd214e11fe453bd22e17a257f3cbea604c9
asmita232 <52271071+asmita232@users.noreply.github.com>
2019-07-06 11:40:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import selenium
-from selnium import webdriver
+from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get("https://www.google.com")
 print("imported selenium successfully")
